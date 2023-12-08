751ed65f3946cc1efa9e32b429ee0ddea59d6db3
ssu-uky <id_suhyun@naver.com>
2023-12-09 02:38:47 +0900

    ✨ feat : User signup, login, logout 기능 생성

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "username",
+        "team",
+        "is_staff",
+        "is_admin",
+    )
+    list_display_links = (
+        "id",
+        "username",
+        "team",
+    )
+    list_filter = ("team",)
+    search_fields = (
+        "username",
+        "team",
+    )
+    # ordering = ("team",)
