be378fef8fe398d95dac3c77a4b168b1da8480f1
ssu-uky <id_suhyun@naver.com>
2023-12-10 22:10:16 +0900

    💡 chore : url 주소 변경

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.TaskListView.as_view(), name="task-list"),
     path("create/", views.CreateTaskView.as_view(), name="task-create"),
-    path("list/", views.TaskListView.as_view(), name="task-list"),
-    path("list/team/", views.MyTaskListView.as_view(), name="task-team-list"),
+    path("myteam/", views.MyTaskListView.as_view(), name="task-team-list"),
     path("<int:pk>/", views.TaskDetailView.as_view(), name="task-detail"),
 ]
